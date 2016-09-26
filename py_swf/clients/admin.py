6e344a765de64c1fde17176be9ee1ce46fa5a33b
Yunong Liu <yliu@yelp.com>
2016-09-26 14:57:11 -0700

    Revert "use staticonf for config, add logger to boto client 1st draft"

--- py_swf/clients/admin.py
+++ py_swf/clients/admin.py
@@ -6,8 +6,6 @@ import functools
 
 from botocore.exceptions import ClientError
 
-from py_swf.clients.util import set_logger_on_boto_client
-
 
 __all__ = ['WorkflowRegistrar']
 
@@ -36,7 +34,6 @@ class WorkflowRegistrar(object):
 
     def __init__(self, boto_client):
         self.boto_client = boto_client
-        set_logger_on_boto_client(self.boto_client)
 
     @idempotent_create
     def register_domain(self, name, description=None, retention=90):
