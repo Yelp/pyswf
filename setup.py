34a05680671d59cf2d2eaf4d488d456c74c574a4
Yunong Liu <yliu@yelp.com>
2016-10-03 14:22:36 -0700

    bump botocore version to newest 1.4.46 on pypi

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     packages=find_packages(exclude=['tests*', 'testing']),
     install_requires=[
         'boto3==1.2.1',
-        'botocore==1.3.7',
+        'botocore==1.4.46',
     ],
     zip_safe=False,
     keywords=['py_swf', 'swf', 'amazon', 'workflow'],
