e7808e1009a4bfebf07ef01aba5f6e437cb1f3cb
Yunong Liu <yliu@yelp.com>
2016-11-14 16:09:42 -0800

    add count open/closed workflow, finish writing unit tests

--- py_swf/clients/workflow.py
+++ py_swf/clients/workflow.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import unicode_literals
+from datetime import datetime
 
 
 __all__ = ['WorkflowClient']
+
+
+class InvalidQueryToCountWorkflow(Exception):
+    pass
+
+
+class MissingRequiredArgs(Exception):
+    pass
 
 
 class WorkflowClient(object):
@@ -65,3 +74,267 @@ class WorkflowClient(object):
             workflowId=workflow_id,
             reason=reason,
         )
+
+    def count_open_workflow_by_time(self, oldest_start_date, latest_start_date=None):
+        """
+        start_date should be type of datetime
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date)
+        return self._count_open_workflow_executions(
+            start_time_filter_dict=time_filter_dict,
+        )
+
+    def count_open_workflow_by_type(self, name, oldest_start_date=None, latest_start_date=None, version=None):
+        """
+        If specify a time range, oldest_start_date is required
+        :param name: string
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :param version: string
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date)
+        type_filter_dict = _build_type_filter_dict(name, version)
+        return self._count_open_workflow_executions(
+            start_time_filter_dict=time_filter_dict,
+            workflow_filter_dict=type_filter_dict,
+        )
+
+    def count_open_workflow_by_tag(self, tag, oldest_start_date=None, latest_start_date=None):
+        """
+        If specify a time range, oldest_start_date is required
+        :param tag: string
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date)
+        tag_filter_dict = _build_tag_filter_dict(tag)
+        return self._count_open_workflow_executions(
+            start_time_filter_dict=time_filter_dict,
+            workflow_filter_dict=tag_filter_dict,
+        )
+
+    def count_open_workflow_by_id(self, workflow_id, oldest_start_date=None, latest_start_date=None):
+        """
+        If specify a time range, oldest_start_date is required
+        :param workflow_id: string
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date)
+        execution_filter_dict = _build_execution_filter_dict(workflow_id)
+        return self._count_open_workflow_executions(
+            start_time_filter_dict=time_filter_dict,
+            workflow_filter_dict=execution_filter_dict,
+        )
+
+    def count_closed_workflow_by_time(self, oldest_start_date=None, latest_start_date=None, oldest_close_date=None, latest_close_date=None):
+        """
+        If specify a time range, either oldest_start_date or oldest_close_date is required.
+        Note that oldest_start_date and oldest_close_date is mutually exclusive.
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :param oldest_close_date: datetime
+        :param latest_close_date: datetime
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date, oldest_close_date=oldest_close_date, latest_close_date=latest_close_date)
+        return self._count_closed_workflow_executions(
+            time_filter_dict=time_filter_dict,
+        )
+
+    def count_closed_workflow_by_type(self, name, version=None, oldest_start_date=None, latest_start_date=None, oldest_close_date=None, latest_close_date=None):
+        """
+        If specify a time range, either oldest_start_date or oldest_close_date is required.
+        Note that oldest_start_date and oldest_close_date are mutually exclusive
+        :param name: string
+        :param version: string
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :param oldest_close_date: datetime
+        :param latest_close_date: datetime
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date, oldest_close_date=oldest_close_date, latest_close_date=latest_close_date)
+        type_filter_dict = _build_type_filter_dict(name, version)
+        return self._count_closed_workflow_executions(
+            time_filter_dict=time_filter_dict,
+            workflow_filter_dict=type_filter_dict,
+        )
+
+    def count_closed_workflow_by_tag(self, tag, oldest_start_date=None, latest_start_date=None, oldest_close_date=None, latest_close_date=None):
+        """
+        If specify a time range, either oldest_start_date or oldest_close_date is required.
+        Note that oldest_start_date and oldest_close_date are mutually exclusive
+        :param tag: string
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :param oldest_close_date: datetime
+        :param latest_close_date: datetime
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date, oldest_close_date=oldest_close_date, latest_close_date=latest_close_date)
+        tag_filter_dict = _build_tag_filter_dict(tag)
+        return self._count_closed_workflow_executions(
+            time_filter_dict=time_filter_dict,
+            workflow_filter_dict=tag_filter_dict,
+        )
+
+    def count_closed_workflow_by_id(self, workflow_id, oldest_start_date=None, latest_start_date=None, oldest_close_date=None, latest_close_date=None):
+        """
+        If specify a time range, either oldest_start_date or oldest_close_date is required.
+        Note that oldest_start_date and oldest_close_date are mutually exclusive
+        :param workflow_id: string
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :param oldest_close_date: datetime
+        :param latest_close_date: datetime
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date, oldest_close_date=oldest_close_date, latest_close_date=latest_close_date)
+        execution_filter_dict = _build_execution_filter_dict(workflow_id)
+        return self._count_closed_workflow_executions(
+            time_filter_dict=time_filter_dict,
+            workflow_filter_dict=execution_filter_dict,
+        )
+
+    def count_closed_workflow_by_close_status(self, status, oldest_start_date=None, latest_start_date=None, oldest_close_date=None, latest_close_date=None):
+        """
+        If specify a time range, either oldest_start_date or oldest_close_date is required.
+        Note that oldest_start_date and oldest_close_date are mutually exclusive
+        :param status: string
+        :param oldest_start_date: datetime
+        :param latest_start_date: datetime
+        :param oldest_close_date: datetime
+        :param latest_close_date: datetime
+        :return:
+        """
+        time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date, oldest_close_date=oldest_close_date, latest_close_date=latest_close_date)
+        close_status_filter_dict = _build_close_status_filter_dict(status)
+        return self._count_closed_workflow_executions(
+            time_filter_dict=time_filter_dict,
+            workflow_filter_dict=close_status_filter_dict,
+        )
+
+    def _count_open_workflow_executions(self, start_time_filter_dict=None, workflow_filter_dict=None):
+        """
+        Count the number of open workflows for a domain. You can pass in filter criteria as a dict
+        :param start_time_filter_dict: dict. key as time filter type and value as date range to filter result.
+        :param workflow_filter_dict: dict. filter type as key and filter criteria as value. filter criteria is a dictionary
+        For more info at http://boto3.readthedocs.io/en/latest/reference/services/swf.html#SWF.Client.count_open_workflow_executions
+        :return:
+        """
+        if not workflow_filter_dict:
+            workflow_filter_dict = {}
+
+        if not start_time_filter_dict:
+            return self.boto_client.count_open_workflow_executions(
+                domain=self.workflow_client_config.domain,
+                **workflow_filter_dict
+            )
+
+        return self.boto_client.count_open_workflow_executions(
+            domain=self.workflow_client_config.domain,
+            startTimeFilter=start_time_filter_dict['startTimeFilter'],
+            **workflow_filter_dict
+        )
+
+    def _count_closed_workflow_executions(self, time_filter_dict=None, workflow_filter_dict=None):
+        """
+        Count the number of closed workflows for a domain. You can pass in filtering criteria as a dict
+        :param time_filter_dict: dict. key as time filter type and value as date range to filter result in a date range.
+        :param workflow_filter_dict: dict. filter type as key and filter criteria as value. filter criteria is a dictionary
+        :return: total number of closed workflows that meet the filter criteria
+        """
+        if not workflow_filter_dict:
+            workflow_filter_dict = {}
+
+        if not time_filter_dict:
+            return self.boto_client.count_closed_workflow_executions(
+                domain=self.workflow_client_config.domain,
+                **workflow_filter_dict
+            )
+
+        for key in time_filter_dict:
+            time_filter_type = key
+        if time_filter_type == 'startTimeFilter':
+            return self.boto_client.count_closed_workflow_executions(
+                domain=self.workflow_client_config.domain,
+                startTimeFilter=time_filter_dict[time_filter_type],
+                **workflow_filter_dict
+            )
+        else:
+            return self.boto_client.count_closed_workflow_executions(
+                domain=self.workflow_client_config.domain,
+                closeTimeFilter=time_filter_dict[time_filter_type],
+                **workflow_filter_dict
+            )
+
+
+def _build_time_filter_dict(oldest_start_date=None, latest_start_date=None, oldest_close_date=None, latest_close_date=None):
+    """
+    Build time_filter_dict for count open/closed workflow executions.
+    Return result is a dict: {time_filter_type : filter_dict}
+    filter_dict must contains key 'oldestDate'.
+    return example:
+    {
+        'startTimeFilter': {
+            'oldestDate': datetime(2016, 11, 11)
+        }
+    }
+    If no oldest_date specified, return None
+    """
+    if oldest_start_date and oldest_close_date:
+        raise InvalidQueryToCountWorkflow("Must specify only oldest_start_date or oldest_close_date!")
+    if oldest_start_date:
+        filter_type = 'startTimeFilter'
+        _validate_date_type(oldest_start_date)
+        filter_dict = {'oldestDate': oldest_start_date}
+        if latest_start_date:
+            _validate_date_type(latest_start_date)
+            filter_dict['latestDate'] = latest_start_date
+    elif oldest_close_date:
+        filter_type = 'closeTimeFilter'
+        _validate_date_type(oldest_close_date)
+        filter_dict = {'oldestDate': oldest_close_date}
+        if latest_close_date:
+            _validate_date_type(latest_close_date)
+            filter_dict['latestDate'] = latest_close_date
+    else:
+        return None
+    return {filter_type: filter_dict}
+
+
+def _validate_date_type(date):
+    if not isinstance(date, datetime):
+        raise InvalidQueryToCountWorkflow("time filter value must be datetime type")
+
+
+def _build_type_filter_dict(name, version):
+    filter_dict = {'name': name}
+    if version:
+        filter_dict['version'] = version
+    return {'typeFilter': filter_dict}
+
+
+def _build_tag_filter_dict(tag):
+    filter_dict = {'tag': tag}
+    return {'tagFilter': filter_dict}
+
+
+def _build_execution_filter_dict(workflow_id):
+    filter_dict = {'workflowId': workflow_id}
+    return {'executionFilter': filter_dict}
+
+
+def _build_close_status_filter_dict(status):
+    valid_status = set(['COMPLETED', 'FAILED', 'CANCELED', 'TERMINATED', 'CONTINUED_AS_NEW', 'TIMED_OUT'])
+    if status not in valid_status:
+        raise InvalidQueryToCountWorkflow("Invalid workflow close status!")
+    filter_dict = {'status': status}
+    return {'closeStatusFilter': filter_dict}
--- tests/unit/clients/workflow_test.py
+++ tests/unit/clients/workflow_test.py
@@ -2,9 +2,16 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import functools
 import mock
 import pytest
 
+from datetime import datetime
+from unittest import TestCase
+
+from py_swf.clients.workflow import _build_time_filter_dict
+from py_swf.clients.workflow import InvalidQueryToCountWorkflow
+from py_swf.clients.workflow import MissingRequiredArgs
 from py_swf.clients.workflow import WorkflowClient
 
 
@@ -54,3 +61,226 @@ def test_terminate_workflow(workflow_config, workflow_client, boto_client):
         workflowId='workflow_id',
         reason='reason',
     )
+
+
+class AssertExceptionRaisedTestHelper(TestCase):
+    def assertion_exception_raised(self, exception_class, func, *args, **kwargs):
+        with self.assertRaises(exception_class):
+            return func(*args, **kwargs)
+
+    def assert_no_exception_raised(self, func, *args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            self.fail("unexpected exception {}".format(repr(e)))
+
+    # unittest.TestCase requires runTest()
+    def runTest(self):
+        pass
+
+
+def test_build_time_filter_dict():
+    time_filter_dict = _build_time_filter_dict()
+    assert time_filter_dict is None
+
+    start_date = datetime(2016, 11, 11)
+    time_filter_dict = _build_time_filter_dict(oldest_start_date=start_date)
+    assert 'startTimeFilter' in time_filter_dict
+    assert time_filter_dict['startTimeFilter'] == {'oldestDate': start_date}
+
+    time_filter_dict = _build_time_filter_dict(oldest_close_date=start_date)
+    assert 'closeTimeFilter' in time_filter_dict
+    assert time_filter_dict['closeTimeFilter'] == {'oldestDate': start_date}
+
+    end_date = datetime(2017, 11, 11)
+    time_filter_dict = _build_time_filter_dict(oldest_start_date=start_date, latest_start_date=end_date)
+    assert 'startTimeFilter' in time_filter_dict
+    assert time_filter_dict['startTimeFilter'] == {'oldestDate': start_date, 'latestDate': end_date}
+
+    time_filter_dict = _build_time_filter_dict(oldest_close_date=start_date, latest_close_date=end_date)
+    assert 'closeTimeFilter' in time_filter_dict
+    assert time_filter_dict['closeTimeFilter'] == {'oldestDate': start_date, 'latestDate': end_date}
+
+    test_helper = AssertExceptionRaisedTestHelper()
+    assert_invalid_datetime_query = functools.partial(
+        test_helper.assertion_exception_raised,
+        exception_class=InvalidQueryToCountWorkflow,
+        func=_build_time_filter_dict,
+    )
+    assert_invalid_datetime_query(oldest_start_date=start_date, oldest_close_date=start_date)
+
+
+def test_count_open_workflow_executions(workflow_config, workflow_client, boto_client):
+    workflow_client._count_open_workflow_executions()
+    _assert_count_open_workflow_executions_called_with(boto_client, workflow_config)
+
+    oldest_start_date = datetime(2016, 11, 11)
+    time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date)
+    workflow_client._count_open_workflow_executions(
+        start_time_filter_dict=time_filter_dict,
+    )
+    _assert_count_open_workflow_executions_called_with(boto_client, workflow_config, startTimeFilter=time_filter_dict['startTimeFilter'])
+
+    workflow_filter_dict = {'test_filter': {'key': 'value'}}
+    workflow_client._count_open_workflow_executions(
+        start_time_filter_dict=time_filter_dict,
+        workflow_filter_dict=workflow_filter_dict,
+    )
+    _assert_count_open_workflow_executions_called_with(
+        boto_client,
+        workflow_config,
+        startTimeFilter=time_filter_dict['startTimeFilter'],
+        test_filter=workflow_filter_dict['test_filter'],
+    )
+
+
+def test_count_closed_workflow_executions(workflow_config, workflow_client, boto_client):
+    oldest_start_date = datetime(2016, 11, 11)
+    oldest_close_date = oldest_start_date
+    start_time_filter_dict = _build_time_filter_dict(oldest_start_date=oldest_start_date)
+    close_time_filter_dict = _build_time_filter_dict(oldest_close_date=oldest_close_date)
+    workflow_filter_dict = {'test_filter': {'key': 'value'}}
+
+    workflow_client._count_closed_workflow_executions()
+    boto_client.count_closed_workflow_executions.assert_called_once_with(domain=workflow_config.domain)
+
+    workflow_client._count_closed_workflow_executions(time_filter_dict=start_time_filter_dict)
+    _assert_count_closed_workflow_executions_called_with(
+        boto_client,
+        workflow_config,
+        startTimeFilter=start_time_filter_dict['startTimeFilter'],
+    )
+
+    workflow_client._count_closed_workflow_executions(time_filter_dict=close_time_filter_dict)
+    _assert_count_closed_workflow_executions_called_with(
+        boto_client,
+        workflow_config,
+        closeTimeFilter=close_time_filter_dict['closeTimeFilter'],
+    )
+
+    workflow_client._count_closed_workflow_executions(time_filter_dict=start_time_filter_dict, workflow_filter_dict=workflow_filter_dict)
+    _assert_count_closed_workflow_executions_called_with(
+        boto_client,
+        workflow_config,
+        startTimeFilter=start_time_filter_dict['startTimeFilter'],
+        test_filter=workflow_filter_dict['test_filter'],
+    )
+
+
+def test_count_open_workflow_by_time(boto_client, workflow_client, workflow_config):
+    oldest_start_date = datetime(2016, 11, 11)
+    workflow_client.count_open_workflow_by_time(oldest_start_date=oldest_start_date)
+    time_dict = dict(oldestDate=oldest_start_date)
+    _assert_count_open_workflow_executions_called_with(boto_client, workflow_config, startTimeFilter=time_dict)
+
+    latest_start_date = datetime(2016, 11, 24)
+    workflow_client.count_open_workflow_by_time(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date)
+    time_dict = dict(oldestDate=oldest_start_date, latestDate=latest_start_date)
+    _assert_count_open_workflow_executions_called_with(boto_client, workflow_config, startTimeFilter=time_dict)
+
+
+def _assert_count_open_workflow_executions_called_with(boto_client, workflow_config, **kwargs):
+    _assert_boto_method_called_with(boto_client.count_open_workflow_executions, workflow_config.domain, **kwargs)
+
+
+def _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config, **kwargs):
+    _assert_boto_method_called_with(boto_client.count_closed_workflow_executions, workflow_config.domain, **kwargs)
+
+
+def _assert_boto_method_called_with(boto_method, domain, **kwargs):
+    boto_method.assert_called_with(
+        domain=domain,
+        **kwargs
+    )
+
+
+def test_count_open_workflow_by_type(boto_client, workflow_client, workflow_config):
+    name = 'test'
+    version = 'test version'
+    workflow_client.count_open_workflow_by_type(name=name)
+    _assert_count_open_workflow_executions_called_with(boto_client, workflow_config, typeFilter=dict(name=name))
+
+    workflow_client.count_open_workflow_by_type(name=name, version=version)
+    _assert_count_open_workflow_executions_called_with(boto_client, workflow_config, typeFilter=dict(name=name, version=version))
+
+
+def test_count_open_workflow_by_tag(boto_client, workflow_client, workflow_config):
+    tag = 'test tag'
+    workflow_client.count_open_workflow_by_tag(tag=tag)
+    _assert_count_open_workflow_executions_called_with(boto_client, workflow_config, tagFilter=dict(tag=tag))
+
+
+def test_count_open_workflow_by_id(boto_client, workflow_client, workflow_config):
+    workflow_id = 'test'
+    workflow_client.count_open_workflow_by_id(workflow_id=workflow_id)
+    _assert_count_open_workflow_executions_called_with(boto_client, workflow_config, executionFilter=dict(workflowId=workflow_id))
+
+
+def test_count_closed_workflow_by_time(boto_client, workflow_client, workflow_config):
+    oldest_start_date = datetime(2016, 11, 11)
+    latest_start_date = datetime(2016, 11, 24)
+    oldest_close_date = datetime(2016, 12, 11)
+    latest_close_date = datetime(2016, 12, 24)
+
+    workflow_client.count_closed_workflow_by_time(oldest_start_date=oldest_start_date)
+    _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config, startTimeFilter=dict(oldestDate=oldest_start_date))
+
+    workflow_client.count_closed_workflow_by_time(oldest_start_date=oldest_start_date, latest_start_date=latest_start_date)
+    _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config, startTimeFilter=dict(oldestDate=oldest_start_date, latestDate=latest_start_date))
+
+    workflow_client.count_closed_workflow_by_time(oldest_close_date=oldest_close_date)
+    _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config,
+                                                         closeTimeFilter=dict(oldestDate=oldest_close_date))
+
+    workflow_client.count_closed_workflow_by_time(oldest_close_date=oldest_close_date,
+                                                  latest_close_date=latest_close_date)
+    _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config,
+                                                         closeTimeFilter=dict(oldestDate=oldest_close_date,
+                                                                              latestDate=latest_close_date))
+
+    test_helper = AssertExceptionRaisedTestHelper()
+    assert_pass_mutally_exclusive_date = functools.partial(
+        test_helper.assertion_exception_raised,
+        exception_class=InvalidQueryToCountWorkflow,
+        func=workflow_client.count_closed_workflow_by_time,
+    )
+    assert_pass_mutally_exclusive_date(oldest_start_date=oldest_start_date, oldest_close_date=oldest_close_date)
+
+
+def test_count_closed_workflow_by_type(boto_client, workflow_client, workflow_config):
+    name = 'test'
+    version = 'test version'
+    workflow_client.count_closed_workflow_by_type(name=name)
+    _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config, typeFilter=dict(name=name))
+
+    workflow_client.count_closed_workflow_by_type(name=name, version=version)
+    _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config,
+                                                         typeFilter=dict(name=name, version=version))
+
+
+def test_count_closed_workflow_by_tag(boto_client, workflow_client, workflow_config):
+    tag = 'test tag'
+    workflow_client.count_closed_workflow_by_tag(tag=tag)
+    _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config, tagFilter=dict(tag=tag))
+
+
+def test_count_closed_workflow_by_id(boto_client, workflow_client, workflow_config):
+    workflow_id = 'test'
+    workflow_client.count_closed_workflow_by_id(workflow_id=workflow_id)
+    _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config,
+                                                         executionFilter=dict(workflowId=workflow_id))
+
+
+def test_count_closed_workflow_by_close_status(boto_client, workflow_client, workflow_config):
+    valid_status = set(['COMPLETED', 'FAILED', 'CANCELED', 'TERMINATED', 'CONTINUED_AS_NEW', 'TIMED_OUT'])
+    for status in valid_status:
+        workflow_client.count_closed_workflow_by_close_status(status)
+        _assert_count_closed_workflow_executions_called_with(boto_client, workflow_config, closeStatusFilter=dict(status=status))
+
+    test_helper = AssertExceptionRaisedTestHelper()
+    assert_invalid_close_state = functools.partial(
+        test_helper.assertion_exception_raised,
+        exception_class=InvalidQueryToCountWorkflow,
+        func=workflow_client.count_closed_workflow_by_close_status,
+    )
+    assert_invalid_close_state(status='invalid status')
