afe26cb8c321c0b9b9a96c0f2ecf2c0b5c1cc7c6
Yunong Liu <yliu@yelp.com>
2017-04-18 15:55:23 -0700

    pass in args instead of dict

--- tests/acceptance/workflow_test.py
+++ tests/acceptance/workflow_test.py
@@ -31,7 +31,7 @@ def start_workflow(workflow_client, workflow_id, workflow_start_to_close_timeout
     )
 
 
-def poll_decision_and_respond(decision_client, response_override_dict=None):
+def poll_decision_and_respond(decision_client, activity_timeout_config_dict=None):
     decision_task = decision_client.poll()
 
     activity_id = str(uuid.uuid4())
@@ -45,7 +45,7 @@ def poll_decision_and_respond(decision_client, response_override_dict=None):
         activity_name='test_activity',
         activity_version='0.1',
         activity_input=simplejson.dumps(activity_input),
-        override_config_dict=response_override_dict,
+        **activity_timeout_config_dict
     )
     return decision_task
 
@@ -109,12 +109,12 @@ def test_workflow_with_start_to_close_timeout(workflow_client, decision_client, 
 def test_workflow_with_activity_timeout(workflow_client, decision_client, activity_task_client):
     workflow_id = str(uuid.uuid4())
     start_workflow(workflow_client, workflow_id, workflow_start_to_close_timeout=100)
-    override_dict = {
+    activity_timeout_config = {
         'schedule_to_close_timeout': 30,
         'schedule_to_start_timeout': 10,
         'start_to_close_timeout': 20,
     }
-    poll_decision_and_respond(decision_client, response_override_dict=override_dict)
+    poll_decision_and_respond(decision_client, activity_timeout_config_dict=activity_timeout_config)
     poll_activity_task_and_respond(activity_task_client)
     poll_decision_and_finish_workflow(decision_client, workflow_client)
 
